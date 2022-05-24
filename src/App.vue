f4282e45015d48cf52dd9c3a76cce758553271ea
Garrett Walker <garbwalk@gmail.com>
2022-05-23 23:21:57 -0500

    Remove Vue starter template asstes/css/ and templates. Tried to whip something up with tailwind, I'm struggling lol. Made a very rough implementation that does successfully tell you how far along your deck is. Made it simpler to pluck the order progress from the ordertracking.json file by changing its structure a bit

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,29 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { onMounted, ref } from 'vue';
+import { RouterView } from "vue-router";
+import { useFetch } from '@vueuse/core'
+
+const currentDeckShipmentProgress = ref();
+const userDeckProgress = ref();
+onMounted(async () => {
+  // Fetch latest steam order date
+  const { data } = await useFetch("/ordertracking.json").json();
+  currentDeckShipmentProgress.value = data.value;
+});
+
+const computeDeckProgress = (deckDetails) => {
+  const { deck, reserveTime } = deckDetails;
+  const preorderEpoch = 1626454800;
+  const bestTime = currentDeckShipmentProgress.value[deck].reserveTime;
+
+  userDeckProgress.value = ((bestTime - preorderEpoch) / (reserveTime - preorderEpoch)) * 100;
+}
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <RouterView @checkDeckProgress="computeDeckProgress" />
+  <p v-if="userDeckProgress">Your SteamDeck is currently {{userDeckProgress}}% of the way there!</p>
 </template>
 
 <style>
-@import "@/assets/base.css";
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,74 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import { computed, defineEmits, ref } from 'vue';
+
+const emit = defineEmits(['checkDeckProgress'])
+
+const model = ref('64GB');
+const region = ref('US');
+const reserveTime = ref();
+
+const deck = computed(() => {
+  return region.value + model.value;
+})
+
+function checkDeckProgress() {
+  if (reserveTime.value) {
+    emit('checkDeckProgress', {
+      deck: deck.value,
+      reserveTime: reserveTime.value
+    });
+  }
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container min-w-full min-h-lg bg-cover bg-center grid place-items-center"
+    style="background-image: url('/src/assets/steamdeckrender1.jpeg')">
+    <h2 class="font-sans text-7xl text-center text-slate-50">
+      Where's muh deck
+    </h2>
+  </div>
+  <div class="container mx-auto max-w-min min-h-min">
+    <div
+      class="container relative bottom-15 mx-auto bg-gray-100 h-32 w-192 rounded-lg shadow-md flex flex-row place-items-center">
+      <div class="w-full md:w-1/6 px-3">
+        <label for="models" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+          Model
+        </label>
+        <select id="models"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          v-model="model">
+          <option value="64GB" selected>64GB</option>
+          <option value="256GB">256GB</option>
+          <option value="512GB">512GB</option>
+        </select>
+      </div>
+
+      <div class="w-full md:w-1/6 px-3">
+        <label for="regions" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+          Region
+        </label>
+        <select id="regions"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          v-model="region">
+          <option value="US">US</option>
+          <option value="UK">UK</option>
+          <option value="EU">EU</option>
+        </select>
+      </div>
+
+      <div class="w-full md:w-1/2 px-3">
+        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="reserve-time">
+          Reserve Time
+        </label>
+        <input v-model="reserveTime"
+          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          id="reserve-time" type="number" placeholder="Reserve Time" />
+      </div>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5"
+        @click="checkDeckProgress">
+        Check Deck
+      </button>
+    </div>
+  </div>
 </template>
